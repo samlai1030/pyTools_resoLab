cb5211f23349639087adf1f43efba358ea69fd9b
samlai <samlai@example.com>
2025-12-05 00:28:46 +0800

    feat: Implement main application logic and UI for SFR analysis

--- run_sfr_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Activate the conda environment
-source /Users/samlai/miniconda3/bin/activate Local
-
-# Run the SFR application (PyQt5 is now used instead of PyQt6)
-/Users/samlai/miniconda3/envs/Local/bin/python /Users/samlai/Local_2/agent_test/SFR_app_v2.py
-
